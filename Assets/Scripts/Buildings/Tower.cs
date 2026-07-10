519bcfc16186e5caa2b8d612534f27536190dafd
Sangwook Na <garidana@gmail.com>
2026-07-10 14:46:39 +0900

    타워 관련 스크립트 위치 이동

--- Assets/Scripts/Buildings/Tower.cs
+++ Assets/Scripts/Buildings/Tower.cs
@@ -1,10 +1,49 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Health))]
+[RequireComponent(typeof(TowerAttack))]
 public class Tower : Building, IDamageable
 {
-    public bool IsDead => throw new System.NotImplementedException();
+
+    [SerializeField] private TowerData _towerData;
+
+    private Health _health;
+    private TowerAttack _attack;
+
+    public bool IsDead => _health == null || _health.IsDead;
+
+    public TowerAttack Attack => _attack;
+
+    private void Awake()
+    {
+        _health = GetComponent<Health>();
+        _attack = GetComponent<TowerAttack>();
+    }
+
+    private void Setup(TowerData data)
+    {
+        data = _towerData;
+        _health.Initialize(data.MaxHealth);
+        _health.Died += HandleDisabled;
+
+        _attack.Initialize(data.Attack);
+
+    }
+
     public void TakeDamage(DamageInfo damage)
     {
         throw new System.NotImplementedException();
+    }
+
+    private void HandleDisabled()
+    {
+        // 공격 중지
+        // 부활 대기 시작
+    }
+
+    public void RestoreAtMorning()
+    {
+        // 체력 완전 회복
+        // 비활성 상태라면 재활성화 정책 확인
     }
 }
--- Assets/Scripts/Buildings/TowerData.cs
+++ Assets/Scripts/Buildings/TowerData.cs
@@ -1,23 +1,24 @@
 using UnityEngine;
 
 
-[CreateAssetMenu(menuName = "TowerAndDragon/TowerData", fileName = "TowerData")]
+[CreateAssetMenu(menuName = "TowerAndDragon/Tower Data", fileName = "TowerData")]
 public class TowerData : ScriptableObject
 {
     [Header("Identity")]
     //[Tooltip("스트링테이블 key. 코드에 직접 이름 문자열을 넣지 않는다.")]
     [SerializeField] private string _nameLocKey;
+
+    [Header("Durability")]
     [SerializeField] private float _maxHealth;
+    [SerializeField] private float _reviveDelay;
 
-    //새끼용 이나 연구를 통한 타워 방어막 부여
-    [Header("Shield (appliable)")]  
-
-    [SerializeField] private bool _isShieldOn;
-    [SerializeField] private float _shieldAmount;
-
+    [Header("Attack")]
+    [SerializeField] private AttackSO _attack;
 
     public string NameLocKey => _nameLocKey;
     public float MaxHealth => _maxHealth;
-    public bool IsShieldOn => _isShieldOn;
-    public float ShieldAmount => _shieldAmount;
+    public float ReviveDelay => _reviveDelay;
+    public AttackSO Attack => _attack;
+    public bool CanAttack => _attack != null; //타워의 활성화 상태
+
 }
