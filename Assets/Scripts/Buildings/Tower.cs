a46ecf86d4fd4a1c4f3bc2dca228b9c4da987911
Sangwook Na <garidana@gmail.com>
2026-07-10 13:47:44 +0900

    타워데이터SO 작업 시작

--- Assets/Scripts/Buildings/Tower.cs
+++ Assets/Scripts/Buildings/Tower.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 
-public class Tower : Building
+public class Tower : Building, IDamageable
 {
-    
+    public bool IsDead => throw new System.NotImplementedException();
+    public void TakeDamage(DamageInfo damage)
+    {
+        throw new System.NotImplementedException();
+    }
 }
