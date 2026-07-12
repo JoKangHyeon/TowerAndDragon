9892e29386eb179788317a1f7b5e037198a39919
나상욱 <garidana@gmail.com>
2026-07-13 00:20:07 +0900

    포탈 Portal.cs와 테스트 씬 작업

--- Assets/Scripts/Portal/Portal.cs
+++ Assets/Scripts/Portal/Portal.cs
@@ -3,13 +3,25 @@ using UnityEngine.Splines;
 
 public class Portal : MonoBehaviour
 {
-    public PortalId Id {get;}
-    public Transform SpawnPoint {get;}
-      
+    [SerializeField] private PortalId _id;
+    [SerializeField] private Transform _spawnPoint;
+    [SerializeField] private SplineContainer _groundPath;
+    [SerializeField] private Transform _mainCastle;
+    [SerializeField] private bool _isActive;
+    //[SerializeField] private bool _isConfigured;
+
+
+    public PortalId Id => _id;
+    public Transform SpawnPoint => _spawnPoint;
+
     // 지상적일 경우 사용할 경로
-    public SplineContainer GroundPath {get;}
-    public Transform MainCastle {get;}
-    public bool IsActive {get;}
-    public bool IsConfigured {get;}
+    public SplineContainer GroundPath => _groundPath;
+    public Transform MainCastle => _mainCastle ;
+    public bool IsActive => _isActive;
+    public bool IsConfigured => 
+        _id != PortalId.None &&
+        _spawnPoint != null &&
+        _groundPath != null &&
+        _mainCastle != null;
 
 }
