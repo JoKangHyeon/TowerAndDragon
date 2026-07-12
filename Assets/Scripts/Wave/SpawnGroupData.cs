bba6bed9d14cc5559456e7bb800f62adb243fda2
나상욱 <garidana@gmail.com>
2026-07-13 00:30:39 +0900

    포탈 웨이브 관련 데이터
    포탈과 포탈에서 나오는 적들의 정보등을 담는 웨이브 데이터

---
+++ Assets/Scripts/Wave/SpawnGroupData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class SpawnGroupData : MonoBehaviour
+{
+    [SerializeField] private BaseMonster _monsterPrefab;
+    [SerializeField] private MonsterData _monsterData;
+    [SerializeField] private int _spawnCount;
+    [SerializeField] private float _spawnInterval;
+    [SerializeField] private float _delayAfterGroup;
+
+    public BaseMonster MonsterPrefab => _monsterPrefab;
+    public MonsterData MonsterData => _monsterData;
+    public int SpawnCount => _spawnCount;
+    public float SpawnInterval => _spawnInterval;
+    public float DelayAfterGroup => _delayAfterGroup;
+
+}
