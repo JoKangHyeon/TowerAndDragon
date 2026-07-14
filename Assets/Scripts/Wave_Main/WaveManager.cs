eb173fe84e4b8fccb81d813faae351e631993c7b
Sangwook Na <garidana@gmail.com>
2026-07-14 11:03:35 +0900

    기반 작업
    - Tooltip 추가
    - 중복 필드 통합

--- Assets/Scripts/Wave_Main/WaveManager.cs
+++ Assets/Scripts/Wave_Main/WaveManager.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
 
-public class WaveDirector : MonoBehaviour
+/// <summary>
+/// 웨이브 실행을 조율한다. 실제 실행 로직은 데이터 계약 검증 후 구현한다.
+/// </summary>
+public class WaveManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+    
 }
