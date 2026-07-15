f19e985f1f9557073a01ae18b3624006530d6d2d
KJH <bmj1644035@gmail.com>
2026-07-15 14:33:49 +0900

    게임오버 처리
    - 게임오버 구현
    - 성 부숴지는 애니메이션 연출 추가
    - 폭탄타워 스프라이트 추가

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -20,6 +21,11 @@ public class GameManager : MonoBehaviour
     public CycleManager CycleManager => _cycleManager;
     public List<CycleLight> DefaultLights => _defaultLights;
 
+    private bool _isGameOver;
+
+    /// <summary>성이 파괴되어 게임오버가 되면 발생. 게임오버 UI 등이 구독한다.</summary>
+    public event Action GameOverOccurred;
+
     private void Awake()
     {
         _cycleManager.Construct(this);
@@ -31,5 +37,17 @@ public class GameManager : MonoBehaviour
         {
             light.Construct(_cycleManager);
         }
+    }
+
+    // 성이 파괴되면 성이 호출한다(중복 호출 무시). 실제 창 표시는 이벤트 구독자가 담당.
+    public void GameOver()
+    {
+        if (_isGameOver)
+        {
+            return;
+        }
+
+        _isGameOver = true;
+        GameOverOccurred?.Invoke();
     }
 }
