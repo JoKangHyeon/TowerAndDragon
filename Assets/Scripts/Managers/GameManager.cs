c27c3543aed9918c8fc461d344e76e453e9eb13c
KJH <bmj1644035@gmail.com>
2026-07-15 14:57:19 +0900

    오류수정

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -21,15 +21,11 @@ public class GameManager : MonoBehaviour
     public CycleManager CycleManager => _cycleManager;
     public List<CycleLight> DefaultLights => _defaultLights;
 
-    private bool _isGameOver;
-
     /// <summary>성이 파괴되어 게임오버가 되면 발생. 게임오버 UI 등이 구독한다.</summary>
     public event Action GameOverOccurred;
 
     private bool _isGameOver;
 
-    /// <summary>성이 파괴되어 게임오버가 되면 발생. 게임오버 UI 등이 구독한다.</summary>
-    public event Action GameOverOccurred;
 
     private void Awake()
     {
